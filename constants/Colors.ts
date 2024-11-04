d0f103116dca2069f14110ab16868e1061f7040d
Aishwar Pathak <shubhum19@gmail.com>
2024-11-04 18:35:48 +0530

    Add tab navigation with Expo Router

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -5,7 +5,7 @@
 
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
-
+const secondaryColor = '#F5F5F5';
 export const Colors = {
   light: {
     text: '#11181C',
@@ -23,4 +23,5 @@ export const Colors = {
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+  PRIMARY : '#7F57F1',
 };
