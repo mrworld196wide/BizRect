5d0d23bb71f20d2237d4de728554c26e05702630
Aishwar Pathak <shubhum19@gmail.com>
2024-11-12 11:37:01 +0530

    implemented oauth

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function home() {
   return (
     <View>
-      <Text style={{ fontSize: 24, fontFamily: "outfit-medium" }}>home</Text>
+      <Text style={{ fontSize: 24, fontFamily: "outfit-medium" }}>Home</Text>
     </View>
   );
 }
--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -1,22 +1,44 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+import * as WebBrowser from "expo-web-browser"; 
 import { Colors } from "@/constants/Colors";
+import { useWarmUpBrowser } from "../hooks/useWarmUpBrowser";
+import { useOAuth } from '@clerk/clerk-expo';
 
+WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
+  useWarmUpBrowser();
+  
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
   return (
     <View>
       <View
         style={{
           display: "flex",
           alignItems: "center",
-          marginTop: 80,
+          marginTop: 100,
         }}
       >
         <Image
           source={require("../assets/images/login.png")}
           style={{
             width: 220,
-            height: 420,
+            height: 440,
             borderRadius: 20,
             borderWidth: 6,
             borderColor: "black",
@@ -47,7 +69,7 @@ export default function LoginScreen() {
           Discover local businesses and promote your own, all in one app!{" "}
         </Text>
       </View>
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={onPress}>
           <Text style={{
             textAlign: "center",
             color: "#fff",
