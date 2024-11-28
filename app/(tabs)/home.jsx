4b8cad64bb93bf5725d8b7b90f3dfb750ec2e118
Aishwar Pathak <shubhum19@gmail.com>
2024-11-28 17:04:54 +0530

    added business slider

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Header from "../../components/Home/Header";
+import Slider from "../../components/Home/Slider";
 
 export default function home() {
   return (
     <View>
       {/* Header */}
-      <Header/>
+      <Header />
       {/* Slider */}
-
+      <Slider />
       {/* Category */}
 
       {/* Popular Business */}
---
+++ components/Home/Slider.jsx
@@ -0,0 +1,53 @@
+import { View, Text, FlatList, Image } from 'react-native'
+import React, { useEffect, useState } from "react";
+import { collection, query, getDocs } from "firebase/firestore";
+import { db } from "../../configs/FirebaseConfig";
+export default function Slider() {
+  const [sliderList, setSliderList] = useState([]);
+  useEffect(() => {
+    getSliderList();
+  }, []);
+  const getSliderList = async () => {
+    setSliderList([]);
+    const q = query(collection(db, "Slider"));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      // appending to array
+      setSliderList((prev) => [...prev, doc.data()]);
+    });
+  };
+  return (
+    <View>
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 20,
+          paddingLeft: 20,
+          paddingTop: 5,
+          marginBottom: 5,
+        }}
+      >
+        #Special for you
+      </Text>
+      <FlatList
+      data={sliderList}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      style={{
+        paddingLeft:20,
+        paddingTop:5
+      }}
+      renderItem={({item,index})=>(
+        <Image source={{uri:item.imageUrl}}
+        style={{
+            width:360,
+            height:150,
+            borderRadius:15,
+            marginRight:20
+        }}/>
+      )}/>
+    </View>
+  );
+}
